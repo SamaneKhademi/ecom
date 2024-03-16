8edeeed91dd5b8652b6ae095412df20398902d36
Samane Khademi <s.khademi1992@gmail.com>
2024-03-17 00:02:13 +0330

    login,logout user

--- shop/urls.py
+++ shop/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.shop, name='home'),
     path('about/', views.about, name='about'),
+    path('login/', views.login_user, name='login'),
+    path('logout/', views.logout_user, name='logout'),
 ]
